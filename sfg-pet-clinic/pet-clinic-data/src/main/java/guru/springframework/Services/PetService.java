e7e25e8f9f2d03f651964db0a6799ae79cf752f8
LanLasaki <64391473+LanLasaki@users.noreply.github.com>
2022-11-15 11:49:06 -0500

    careated crudservice and cleaned up service layer

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/Services/PetService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/Services/PetService.java
@@ -1,11 +1,7 @@
 package guru.springframework.Services;
 
 import guru.springframework.model.Pet;
-import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet,Long>{
 
 }
--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/Services/VetService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/Services/VetService.java
@@ -1,11 +1,7 @@
 package guru.springframework.Services;
 
 import guru.springframework.model.Vet;
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService <Vet, Long> {
 
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
