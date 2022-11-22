b8fa48ba20b2e45fd8790f500b2f5a2d8289ee88
LanLasaki <64391473+LanLasaki@users.noreply.github.com>
2022-11-22 17:56:38 -0500

    Added Visit and Speciality services

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/services/map/VisitServiceMap.java
@@ -0,0 +1,44 @@
+package guru.springframework.services.map;
+
+
+
+import guru.springframework.services.VisitService;
+import guru.springframework.model.Visit;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null ||
+        visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("This is an invalid visit.");
+        }
+
+        return super.save( visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+}
